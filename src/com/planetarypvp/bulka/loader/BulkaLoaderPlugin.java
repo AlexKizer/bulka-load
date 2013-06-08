31973164da3adc89b686bc15f847fbceca4924e0
Alex Kizer <the.alex.kizer@gmail.com>
2013-06-08 15:23:40 -0500

    init

---
+++ src/com/planetarypvp/bulka/loader/BulkaLoaderPlugin.java
@@ -0,0 +1,16 @@
+package com.planetarypvp.bulka.loader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BulkaLoaderPlugin extends JavaPlugin
+{
+    public void onEnable()
+    {
+
+    }
+
+    public void onDisable()
+    {
+
+    }
+}
---
+++ src/com/planetarypvp/bulka/loader/ConfigurableClass.java
@@ -0,0 +1,11 @@
+package com.planetarypvp.bulka.loader;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexkizer
+ * Date: 6/8/13
+ * Time: 12:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ConfigurableClass {
+}
